edfd945feb319c1460b5bad7f722eece099ad510
Onno Schuit <o.schuit@solin.nl>
2020-08-05 15:32:19 +0200

    Increasing version number of theme kiwibasis

--- theme/kiwibasis/version.php
+++ theme/kiwibasis/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016022600; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2020080500; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500; // Requires this Moodle version
 $plugin->component = 'theme_kiwibasis'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = [                                                                                                           
